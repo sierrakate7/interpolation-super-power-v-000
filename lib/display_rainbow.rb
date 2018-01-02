15af399a1b46fbc89b307e5f23f6d89d09994934
Sierra McCahon <smitche4@mail.umw.edu>
2018-01-02 22:41:23 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 def display_rainbow (colors_array)
  puts "R: #{colors_array[0]}, O: #{colors_array[1]}, Y: #{colors_array[2]}, G: #{colors_array[3]}, B: #{colors_array[4]}, I: #{colors_array[5]}, V: #{colors_array[6]}"
  end
+=======
+def displat_rainbow
+  puts "R: #{red}, O: #{orange}, Y: #{yellow}, G: #{green}, B: #{blue}, I: #{indigo}, V: #{violet}"
+end
+>>>>>>> 9a5aee00477f573f785f5cedf7c7f22485b7e4ac
